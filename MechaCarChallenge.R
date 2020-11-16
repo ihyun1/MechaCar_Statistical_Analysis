88e49999f96e21f78dba404ead2dc3478ad167bd
ihyun1 <68250704+ihyun1@users.noreply.github.com>
2020-11-15 23:50:03 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,36 +1,39 @@
 library(tidyverse)
 
-### Perform Multiple Regression on MechaCar dataset
-# Read in the csv file.
+# Linear regression for MPG
+
+# Import/read csv
 mecha_data <- read.csv("MechaCar_mpg.csv",stringsAsFactors = F,check.names = F)
 
-# Perform a linear regression module 
+# Linear regression module 
 mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_data)
 
-# Determine the p-value and r-squared of the linear regression module.
+# P-value and r-squared of linear regression
 summary(mecha_lm)
 
-### Perform Summary Analysis on suspension coil dataset
-# Read in the csv file
+# Trip analysis
+
+# Read csv
 suspension_data <- read.csv("Suspension_Coil.csv",stringsAsFactors = F, check.names = F)
 
-# Create the Total Summary
+# Total summary DF
 total_summary <- suspension_data %>% 
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
-# Create the Summary By Lot
+# Lot summary
 lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>% 
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
 
-### Suspension Coil T-Tests
-# Peform t-test across all Lots
+# Suspension coil t-tests
+
+# T-test for all Lots
 t.test(suspension_data$PSI,mu = 1500)
 
-# Peform t-test on Lot 1
+# T-test Lot 1
 t.test(subset(suspension_data,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
 
-# Peform t-test on Lot 2
+# T-test Lot 2
 t.test(subset(suspension_data,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
 
-# Peform t-test on Lot 3
+# T-test Lot 3
 t.test(subset(suspension_data,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
